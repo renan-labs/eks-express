e0bb0c5273e9ba73860a53c323fa789d6cb645c3
Renan Dias <renandias411@gmail.com>
2026-03-14 23:24:15 -0300

    Aula 06-Metrics Server (Addon)

--- cluster/eks.cluster.access.tf
+++ cluster/eks.cluster.access.tf
@@ -1,31 +1,15 @@
-resource "aws_eks_access_entry" "bash_user" {
+resource "aws_eks_access_entry" "iamadmin" {
   cluster_name      = aws_eks_cluster.this.name
-  principal_arn     = local.bash_user_arn
+  principal_arn     = local.eks_access_user_arn
   type              = "STANDARD"
 }
 
-resource "aws_eks_access_entry" "console_user" {
-  cluster_name      = aws_eks_cluster.this.name
-  principal_arn     = local.console_user_arn
-  type              = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "bash_user" {
+resource "aws_eks_access_policy_association" "iamadmin" {
   cluster_name  = aws_eks_cluster.this.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = local.bash_user_arn
+  principal_arn = local.eks_access_user_arn
 
   access_scope {
-    type       = "namespace"
-  }
-}
-
-resource "aws_eks_access_policy_association" "console_user" {
-  cluster_name  = aws_eks_cluster.this.name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = local.console_user_arn
-
-  access_scope {
-    type       = "namespace"
+    type       = "cluster"
   }
 }
--- cluster/locals.tf
+++ cluster/locals.tf
@@ -1,4 +1,3 @@
 locals {
-  bash_user_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/iamadmin"
-  console_user_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/iamadmin"
+  eks_access_user_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/iamadmin"
 }
